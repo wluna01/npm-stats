a43e942ee16a9cf7915dd7ccbb841a911b1995ec
Will Luna <55769826+wluna01@users.noreply.github.com>
2021-02-09 20:13:27 -0800

    version 1 completed
    added top packages and asyn function

--- index.js
+++ index.js
@@ -7,14 +7,26 @@ console.log("starting script");
 var plugins = [
     "sanity-plugin-asset-source-unsplash", 
     "sanity-plugin-media", 
-    "sanity-plugin-content-calendar"
+    "sanity-plugin-content-calendar", 
+    "sanity-plugin-asset-source-cloudinary", 
+    "sanity-plugin-google-analytics",
+    "sanity-plugin-seo-tools",
+    "sanity-plugin-dashboard-widget-gatsby",
+    "sanity-graphql-schema",
+    "strapi",
+    "@sanity/client"
     // add additional plugins here
-    // should be stored in separate .txt file
+    // should be stored in separate .txt file eventually
 ];
 
+var start = new Date('2020-06-01');
+var end = new Date('2021-02-07');
+
+var num_days = ((end.getTime() - start.getTime())  / (1000 * 3600 * 24)) + 1; 
+console.log(plugins.length * num_days);
+var entry_length = plugins.length * num_days;
+
 function pullStats(packageName) {
-    var start = new Date('2021-01-09');
-    var end = new Date('2021-01-09');
     stats.get(start, end, packageName)
     .on('error', console.error)
     .on('data', function(data) {
@@ -23,7 +35,7 @@ function pullStats(packageName) {
     })
     .on('end', function() {
         console.log(exportArray.length);
-        if(exportArray.length == plugins.length) {
+        if(exportArray.length == entry_length) {
             (async () => {
                 const csv = new ObjectsToCsv(exportArray);
                 await csv.toDisk('./test.csv');
